a82cb5b01e1f60e7547a0f9f6a0c4228afdb4a5e
ayerest <irisiseve@gmail.com>
2020-01-06 17:47:52 -0800

    split up auth flow to solve signup bug

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,58 +1,58 @@
 import {AsyncStorage} from 'react-native';
-import { base1 } from '../../env';
+import { base1 } from '../../env'; 
 
-export const signup = (username, password, avatar) => {
-    if (!!avatar) {
-        return async dispatch => {
-            const response = await fetch(`${base1}/users`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    user: {
+export const createAccount = (username, password, avatar) => {
+    return async dispatch => {
+        const response = await fetch(`${base1}/users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user: {
+                username: username,
+                password: password,
+                avatar: avatar
+                }
+            })
+        })
+        if (!response.ok) {
+            const errorData = await response.json();
+            const errorType = errorData.error;
+            throw new Error(errorType);
+        }
+
+        const signupData = await response.json();
+        dispatch({ type: "SIGNUP", payload: signupData});
+        const expirationDate = new Date(new Date().getTime() + 1200000);
+        persistDataToStorage(signupData.jwt, signupData.user, expirationDate);
+    }
+}
+    
+export const userLogin = (username, password) => {
+    return async dispatch => {
+        const response = await fetch(`${base1}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user: {
                     username: username,
                     password: password,
-                    avatar: avatar
-                    }
-                })
+                }
             })
-            if (!response.ok) {
-                const errorData = await response.json();
-                const errorType = errorData.error
-                throw new Error(errorType)
-            }
-
-            const signupData = await response.json();
-            dispatch({ type: "SIGNUP", payload: signupData})
-            const expirationDate = new Date(new Date().getTime() + 1200000)
-            persistDataToStorage(signupData.jwt, signupData.user, expirationDate)
+        })
+        if (!response.ok) {
+            const errorData = await response.json();
+            const errorType = errorData.message;
+            throw new Error(errorType);
         }
-    } else {
-        return async dispatch => {
-            const response = await fetch(`${base1}/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    user: {
-                        username: username,
-                        password: password,
-                    }
-                })
-            })
-            if (!response.ok) {
-                const errorData = await response.json();
-                const errorType = errorData.message
-                throw new Error(errorType)
-            }
 
-            const loginData = await response.json();
-            dispatch({ type: "LOGIN", payload: loginData })
-            const expirationDate = new Date(new Date().getTime() + 12000000)
-            persistDataToStorage(loginData.jwt, loginData.user, expirationDate)
-        }
+        const loginData = await response.json();
+        dispatch({ type: "LOGIN", payload: loginData });
+        const expirationDate = new Date(new Date().getTime() + 12000000);
+        persistDataToStorage(loginData.jwt, loginData.user, expirationDate);
     }
 }
 
