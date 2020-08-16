d7a3cf79b9d3a1ddf82879d3093417abb9486b4c
ayerest <irisiseve@gmail.com>
2020-08-16 12:41:06 -0700

    updates for linter on pictures screen

--- screens/PicturesScreen.js
+++ screens/PicturesScreen.js
@@ -6,12 +6,41 @@ import {
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import uuid from 'uuid';
+import PropTypes from 'prop-types';
 import MenuButton from '../components/MenuButton';
 import Colors from '../constants/Colors';
 import { getMyPhotos } from '../store/actions/photos';
 import { getMyEntries } from '../store/actions/entries';
 import AvatarButton from '../components/AvatarButton';
 import Card from '../components/Card';
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 15,
+  },
+  image: {
+    height: 300,
+    width: '100%',
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: 'black',
+    resizeMode: 'cover',
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    marginRight: 10,
+    alignSelf: 'center',
+    fontFamily: 'Roboto-Condensed',
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+  },
+});
 
 const PicturesScreen = ({ navigation }) => {
   const photosList = useSelector((state) => (state.photos.myPhotos || false));
@@ -23,6 +52,9 @@ const PicturesScreen = ({ navigation }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const handleLeaving = () => {
+      setDisplayIndex(0);
+    };
     const unsubscribe = navigation.addListener('blur', () => {
       handleLeaving();
     });
@@ -31,30 +63,20 @@ const PicturesScreen = ({ navigation }) => {
   }, [navigation]);
 
   useEffect(() => {
+    const loadMyPhotos = async () => {
+      await dispatch(getMyPhotos());
+      setPhotosLoading(false);
+    };
+    const handlePageLoad = () => {
+      setPhotosLoading(true);
+      loadMyPhotos();
+    };
     const subscribe = navigation.addListener('focus', () => {
       handlePageLoad();
     });
 
     return subscribe;
-  }, [navigation]);
-
-  useEffect(() => {
-    let mounted = true;
-    if (mounted) {
-      setPhotosLoading(true);
-      loadMyPhotos();
-    }
-    return () => mounted = false;
-  }, [dispatch]);
-
-  const loadMyPhotos = async () => {
-    await dispatch(getMyPhotos());
-    setPhotosLoading(false);
-  };
-
-  const handlePageLoad = () => {
-    loadMyPhotos();
-  };
+  }, [dispatch, navigation]);
 
   useEffect(() => {
     let mounted = true;
@@ -66,7 +88,9 @@ const PicturesScreen = ({ navigation }) => {
       };
       loadMyFieldEntries();
     }
-    return () => mounted = false;
+    return () => {
+      mounted = false;
+    };
   }, [dispatch]);
 
   const renderPhotoItem = (image) => {
@@ -99,7 +123,6 @@ const PicturesScreen = ({ navigation }) => {
     } else if (indexDiff < 10) {
       setDisplayIndex(displayIndex + indexDiff);
     } else {
-      const temp = displayIndex;
       setDisplayIndex(displayIndex + 10);
     }
   };
@@ -108,25 +131,36 @@ const PicturesScreen = ({ navigation }) => {
     setDisplayIndex(0);
   };
 
-  const handleLeaving = () => {
-    setDisplayIndex(0);
-  };
-
   return (
     <View style={styles.screen}>
-      {photosLoading && entriesLoading ? <ActivityIndicator size="large" color={Colors.linkColor} />
-        : !photosList ? <Text style={styles.label}>You haven't taken any photos yet!</Text>
-          : (
+      {photosLoading && entriesLoading && <ActivityIndicator size="large" color={Colors.linkColor} />}
+      {!photosLoading && !entriesLoading && !photosList
+      && <Text style={styles.label}>You haven`&apos;`t taken any photos yet!</Text>}
+      {!photosLoading && !entriesLoading && photosList
+            && (
             <View>
               <View style={styles.row}>
                 <Button title="Older" onPress={loadMorePhotos} />
                 {displayIndex > 0 ? <Button title="Recent" onPress={loadRecentPhotos} /> : null}
               </View>
-              <FlatList keyExtractor={(item, index) => uuid()} data={photosList.slice(displayIndex, displayIndex + 10)} renderItem={renderPhotoItem} numColumns={1} />
+              <FlatList
+                keyExtractor={() => uuid()}
+                data={photosList.slice(displayIndex, displayIndex + 10)}
+                renderItem={renderPhotoItem}
+                numColumns={1}
+              />
             </View>
-          )}
+            )}
     </View>
   );
+};
+
+PicturesScreen.defaultProps = {
+  navigation: '',
+};
+
+PicturesScreen.propTypes = {
+  navigation: PropTypes.instanceOf(Object),
 };
 
 export const screenOptions = (navData) => ({
@@ -150,34 +184,6 @@ export const screenOptions = (navData) => ({
     }}
     />
   ),
-});
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: 15,
-  },
-  image: {
-    height: 300,
-    width: '100%',
-    borderRadius: 15,
-    borderWidth: 2,
-    borderColor: 'black',
-    resizeMode: 'cover',
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 5,
-    marginRight: 10,
-    alignSelf: 'center',
-    fontFamily: 'Roboto-Condensed',
-  },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
-    justifyContent: 'space-between',
-  },
 });
 
 export default PicturesScreen;
