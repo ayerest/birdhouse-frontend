8d4e3b367013e2c41a8ecb7d17d73096686c3505
ayerest <irisiseve@gmail.com>
2020-08-16 12:44:02 -0700

    updates for linter on startup screen

--- screens/StartupScreen.js
+++ screens/StartupScreen.js
@@ -1,49 +1,51 @@
-import React, {useEffect} from 'react';
-import {View, ActivityIndicator, StyleSheet, AsyncStorage} from 'react-native';
-import {useDispatch} from 'react-redux';
+import React, { useEffect } from 'react';
+import {
+  View, ActivityIndicator, StyleSheet, AsyncStorage,
+} from 'react-native';
+import { useDispatch } from 'react-redux';
 import * as authActions from '../store/actions/auth';
 import Colors from '../constants/Colors';
 
-const StartupScreen = props => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        let mounted = true;
-        if (mounted) {
-            const tryLogin = async () => {
-                const userData = await AsyncStorage.getItem("userData")
-                if (!userData) {
-                    await dispatch(authActions.setAutologin());
-                    return () => mounted = false;
-                } 
-                const transformedData = JSON.parse(userData);
-                const {token, userId, expiryDate} = transformedData;
-                const expirationDate = new Date(expiryDate)
-            
-                if (expirationDate <= new Date() || !token || !userId ) {
-                    await dispatch(authActions.setAutologin());
-                    return () => mounted = false;
-                }
-                await dispatch(authActions.authenticate(userId, token))
-                return () => mounted = false;
-            };
-            tryLogin();
-        }
-        return () => mounted = false;
-    }, [dispatch])
-
-    return (
-        <View style={styles.screen}>
-            <ActivityIndicator size='large' color={Colors.linkColor}/>
-        </View>
-    )
-}
-
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const StartupScreen = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      const tryLogin = async () => {
+        const userData = await AsyncStorage.getItem('userData');
+        if (!userData) {
+          await dispatch(authActions.setAutologin());
+          return () => { mounted = false; };
+        }
+        const transformedData = JSON.parse(userData);
+        const { token, userId, expiryDate } = transformedData;
+        const expirationDate = new Date(expiryDate);
+
+        if (expirationDate <= new Date() || !token || !userId) {
+          await dispatch(authActions.setAutologin());
+          return () => { mounted = false; };
+        }
+        await dispatch(authActions.authenticate(userId, token));
+        return () => { mounted = false; };
+      };
+      tryLogin();
     }
-})
+    return () => { mounted = false; };
+  }, [dispatch]);
+
+  return (
+    <View style={styles.screen}>
+      <ActivityIndicator size="large" color={Colors.linkColor} />
+    </View>
+  );
+};
 
 export default StartupScreen;
