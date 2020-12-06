66539ae5b207a490fb3c9e4c96348e43abd177d2
ayerest <irisiseve@gmail.com>
2020-12-06 10:16:39 -0800

    removed unused import in home screen navigator

--- navigation/HomeScreenNavigator.js
+++ navigation/HomeScreenNavigator.js
@@ -4,41 +4,36 @@ import MainViewScreen from '../screens/MainViewScreen';
 import GeoMap from '../components/GeoMap';
 import StaticMap from '../components/StaticMap';
 import FieldEntryDetailsScreen from '../screens/FieldEntryDetailsScreen';
-import BirdDetailsScreen, {
-  screenOptions as birdDetailsScreenOptions,
-} from '../screens/BirdDetailsScreen';
+import BirdDetailsScreen from '../screens/BirdDetailsScreen';
 import AddFieldEntryForm from '../components/AddFieldEntryForm';
 import screenOptions from './ScreenOptions';
 import defaultNavOptions from './DefaultNavOptions';
 
 const HomeScreenStackNavigator = createStackNavigator();
 
-const HomeScreenNavigator = () => {
-  return (
-    <HomeScreenStackNavigator.Navigator screenOptions={defaultNavOptions}>
-      <HomeScreenStackNavigator.Screen
-        name="BirdHouse"
-        component={MainViewScreen}
-        options={screenOptions}
-      />
-      <HomeScreenStackNavigator.Screen name="Live Map" component={GeoMap} />
-      <HomeScreenStackNavigator.Screen name="Map View" component={StaticMap} />
-      <HomeScreenStackNavigator.Screen
-        name="Field Sighting Details"
-        component={FieldEntryDetailsScreen}
-        options={screenOptions}
-      />
-      <HomeScreenStackNavigator.Screen
-        name="Add Entry"
-        component={AddFieldEntryForm}
-      />
-      <HomeScreenStackNavigator.Screen
-        name="Bird Details"
-        component={BirdDetailsScreen}
-        options={birdDetailsScreenOptions}
-      />
-    </HomeScreenStackNavigator.Navigator>
-  );
-};
+const HomeScreenNavigator = () => (
+  <HomeScreenStackNavigator.Navigator screenOptions={defaultNavOptions}>
+    <HomeScreenStackNavigator.Screen
+      name="BirdHouse"
+      component={MainViewScreen}
+      options={screenOptions}
+    />
+    <HomeScreenStackNavigator.Screen name="Live Map" component={GeoMap} />
+    <HomeScreenStackNavigator.Screen name="Map View" component={StaticMap} />
+    <HomeScreenStackNavigator.Screen
+      name="Field Sighting Details"
+      component={FieldEntryDetailsScreen}
+      options={screenOptions}
+    />
+    <HomeScreenStackNavigator.Screen
+      name="Add Entry"
+      component={AddFieldEntryForm}
+    />
+    <HomeScreenStackNavigator.Screen
+      name="Bird Details"
+      component={BirdDetailsScreen}
+    />
+  </HomeScreenStackNavigator.Navigator>
+);
 
 export default HomeScreenNavigator;
