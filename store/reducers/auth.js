41adda1c1e8d802f87238ea797cdfacf2ccaee01
ayerest <irisiseve@gmail.com>
2020-12-20 09:09:31 -0800

    updating spacing in reducers

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -1,44 +1,46 @@
-import { LOGIN, LOGOUT, AUTHENTICATE, SIGNUP, SET_AUTOLOGIN } from '../actions/auth';
+import {
+  LOGIN, LOGOUT, AUTHENTICATE, SIGNUP, SET_AUTOLOGIN,
+} from '../actions/auth';
 
 const initialState = {
-    user: null,
-    token: null,
-    didTryAutoLogin: false
-}
+  user: null,
+  token: null,
+  didTryAutoLogin: false,
+};
 
-const authReducer = (state=initialState, action) => {
-    switch(action.type) {
-        case SIGNUP:
-            return {
-                didTryAutoLogin: true,
-                user: action.payload.user,
-                token: action.payload.jwt
-            }
-        case LOGIN:
-            return {
-                user: action.payload.user,
-                token: action.payload.jwt,
-                didTryAutoLogin: true
-            }
-        case AUTHENTICATE:
-            return {
-                user: action.payload.user,
-                token: action.payload.token,
-                didTryAutoLogin: true
-            }
-        case SET_AUTOLOGIN:
-            return {
-                ...state,
-                didTryAutoLogin: true
-            }
-        case LOGOUT:
-            return {
-                ...initialState,
-                didTryAutoLogin: true
-            };
-        default:
-            return state;
-    }
-}
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNUP:
+      return {
+        didTryAutoLogin: true,
+        user: action.payload.user,
+        token: action.payload.jwt,
+      };
+    case LOGIN:
+      return {
+        user: action.payload.user,
+        token: action.payload.jwt,
+        didTryAutoLogin: true,
+      };
+    case AUTHENTICATE:
+      return {
+        user: action.payload.user,
+        token: action.payload.token,
+        didTryAutoLogin: true,
+      };
+    case SET_AUTOLOGIN:
+      return {
+        ...state,
+        didTryAutoLogin: true,
+      };
+    case LOGOUT:
+      return {
+        ...initialState,
+        didTryAutoLogin: true,
+      };
+    default:
+      return state;
+  }
+};
 
 export default authReducer;
--- store/reducers/badges.js
+++ store/reducers/badges.js
@@ -1,19 +1,19 @@
 import { MY_BADGES } from '../actions/badges';
 
 const initialState = {
-    myBadges: []
-}
+  myBadges: [],
+};
 
 const badgesReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case MY_BADGES:
-            return {
-                ...state,
-                myBadges: action.myBadges
-            };
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case MY_BADGES:
+      return {
+        ...state,
+        myBadges: action.myBadges,
+      };
+    default:
+      return state;
+  }
+};
 
 export default badgesReducer;
